1913a8c7dad78b5769f3a3d68bc073655a69458b
Vikas Verma <vikas.verma@techplusinfotech.com>
2024-10-15 10:26:36 +0530

    updated code

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
+dotenv.config();
+const db = require("./src/config/db");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const swaggerUi = require("swagger-ui-express");
-const swaggerSpec = require("./swagger"); // Ensure this is correctly set up
-
-dotenv.config();
-
 const app = express();
+const router = require("./src/routes");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./swagger");
 
-// CORS options
+// Middleware
+
 const corsOptions = {
   origin: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
@@ -17,45 +18,15 @@ const corsOptions = {
   credentials: true,
 };
 
-// Middleware setup
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cors()); // enable CORS
 
-// Serve Swagger UI files
-app.use("/api-docs", express.static("public/api-docs"));
-
-// Setup Swagger UI to use the local files
-app.get("/api-docs", (req, res) => {
-  res.send(`
-    <html>
-      <head>
-        <link rel="stylesheet" type="text/css" href="swagger-ui.css" />
-      </head>
-      <body>
-        <div id="swagger-ui"></div>
-        <script src="swagger-ui-bundle.js"></script>
-        <script>
-          const ui = SwaggerUIBundle({
-            url: '/swagger.json', // Adjust the path to your swagger.json
-            dom_id: '#swagger-ui',
-            presets: [
-              SwaggerUIBundle.presets.apis,
-              SwaggerUIStandalonePreset
-            ],
-            layout: "StandaloneLayout",
-          });
-        </script>
-      </body>
-    </html>
-  `);
-});
-
-// Route setup
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/v1", router);
 
-// Start the server
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
