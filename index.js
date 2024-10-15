6737887c1270cc93dda40c1ba775272e6d394a10
Vikas Verma <vikas.verma@techplusinfotech.com>
2024-10-15 10:02:25 +0530

    updated code

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
-dotenv.config();
-const db = require("./src/config/db");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const app = express();
-const router = require("./src/routes");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./swagger");
+const router = require("./src/routes");
+const db = require("./src/config/db"); // Ensure your DB config is being used
 
-// Middleware
+dotenv.config();
 
+const app = express();
+
+// CORS options
 const corsOptions = {
   origin: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
@@ -18,15 +19,28 @@ const corsOptions = {
   credentials: true,
 };
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(cors()); // enable CORS
+// Middleware setup
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// Serve static files for Swagger UI
+app.use("/public", express.static("public"));
+
+// Swagger UI options
+const options = {
+  customCssUrl: "/public/css/swagger-ui.css", // Use absolute path for serving CSS
+  customSiteTitle: "The Words That I Know API - Swagger",
+};
+
+// Serve Swagger UI
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, options));
+
+// Route setup
 app.use("/v1", router);
 
+// Start the server
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
